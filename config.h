0e6fa85239d86102dd2e51971947d96d99f7a2bd
ShellCode33 <shellcode33@protonmail.ch>
2022-11-21 01:08:00 +0100

    Change font settings

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fantasque Sans Mono:pixelsize=20:antialias=true:autohint=true";
-static char *font2[] = {"Noto Color Emoji:pixelsize=18:antialias=true:autohint=true"};
+static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = {"Noto Color Emoji:pixelsize=15:antialias=true:autohint=true"};
 
 static int borderpx = 2;
 
